8e0ceed5b4b04c5c127b2bedecf57cdfa2258e07
Lee <L918Sturrock@hotmail.com>
2019-11-12 16:04:56 +0000

    Added hand models and weapon functionality scripts

---
+++ FallenSpace/Assets/Scripts/Weapon/Gun.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 30f;
+
+    public Camera fpsCam;
+
+    public Animator gunAnimator;
+
+    // particles ***************
+    public ParticleSystem Muzzleflash;
+    public GameObject impact;
+    // *************************
+
+    private float nextTimeToFire = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        gunAnimator.SetTrigger("FirePistol");
+        Muzzleflash.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactParticle = Instantiate(impact, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactParticle, 0.40f);
+        }
+
+
+    }
+}
---
+++ FallenSpace/Assets/WeaponSwitching.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+
+public class WeaponSwitching : MonoBehaviour
+{
+    public int selectedWeapon = 0;
+
+    void Awake()
+    {
+        selectWeapon();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        int previousSelectedWeapon = selectedWeapon;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (selectedWeapon >= transform.childCount - 1)
+            {
+                selectedWeapon = 0; // sword
+            }
+            else
+            {
+                selectedWeapon++; // 1 - machinegun, 2 - shotgun.
+            }
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (selectedWeapon <= 0)
+            {
+                selectedWeapon = transform.childCount - 1;
+            }
+            else
+            {
+                selectedWeapon--;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) // sword
+        {
+            selectedWeapon = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2) // gun
+        {
+            selectedWeapon = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3) // shotgun
+        {
+            selectedWeapon = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 4) // ??
+        {
+            selectedWeapon = 3;
+        }
+
+        if (previousSelectedWeapon != selectedWeapon)
+        {
+            selectWeapon();
+        }
+    }
+
+    void selectWeapon()
+    {
+        int i = 0;
+
+        foreach (Transform weapon in transform)
+        {
+            if (i == selectedWeapon)
+            {
+                weapon.gameObject.SetActive(true);
+            }
+            else
+            {
+                weapon.gameObject.SetActive(false);
+            }
+
+            i++;
+        }
+    }
+}
