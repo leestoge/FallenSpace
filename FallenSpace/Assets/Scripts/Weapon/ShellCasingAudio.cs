fcb52d2f27a150d5a8b08a1db6179f50d1631d5b
Lee <L918Sturrock@hotmail.com>
2020-02-12 02:22:10 +0000

    Organizing assets into folders, deleting unnecessary imports.

---
+++ FallenSpace/Assets/Scripts/Weapon/ShellCasingAudio.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ShellCasingAudio : MonoBehaviour
+{
+
+    private bool audioPlayed;
+
+
+    void Awake()
+    {
+        audioPlayed = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        if (audioPlayed == false)
+        {
+            FindObjectOfType<AudioManager>().RandomizePitch("shellCasing");
+            FindObjectOfType<AudioManager>().Play("shellCasing");
+            audioPlayed = true;
+        }      
+    } 
+}
