26972db38b66d3a19fb00e70d85f039141f3e2b5
Lee <L918Sturrock@hotmail.com>
2020-03-04 00:05:47 +0000

    Created PlayerAmmo script for the player UIs ammo count to function

---
+++ FallenSpace/Assets/Scripts/UI/PlayerAmmo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerAmmo : MonoBehaviour
+{
+    public Text ammoOnScreen;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ammoOnScreen.text = FindObjectOfType<Gun>().currentAmmo.ToString();
+    }
+}
