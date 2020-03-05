9ed70f3281c056a79329ceb00f614754192bc3a4
Lee <L918Sturrock@hotmail.com>
2020-03-05 03:34:41 +0000

    Setup teleporter related scripts directory, will hopefully flesh out tomorrow

---
+++ FallenSpace/Assets/Scripts/World/Teleporters/Teleporter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public Transform teleportTarget;
+    private GameObject _thePlayer;
+
+    void Awake()
+    {
+        _thePlayer = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        _thePlayer.transform.position = teleportTarget.transform.position;
+    }
+}
