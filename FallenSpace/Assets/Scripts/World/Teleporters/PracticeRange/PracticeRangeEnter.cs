eb13d631445511f2b6142b805f2ad1d3f49024fe
Lee <L918Sturrock@hotmail.com>
2020-03-05 19:44:09 +0000

    Updated practice range enter script to display relevant ui when in trigger

--- FallenSpace/Assets/Scripts/World/Teleporters/PracticeRange/PracticeRangeEnter.cs
+++ FallenSpace/Assets/Scripts/World/Teleporters/PracticeRange/PracticeRangeEnter.cs
@@ -1,18 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PracticeRangeEnter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject canvasScore;
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            canvasScore.SetActive(true);
+        }
     }
 }
